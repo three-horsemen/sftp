84b43d24e5f7135076134003c762b0ba5ee0724b
soorya-annadurai <soorya.annadurai@gmail.com>
2016-09-27 00:31:19 +0530

    Added RSA asymmetric cryptographic functions to "rsa.hpp". Separated cryptographic mathematical functions into "cryptmath.hpp". Made unit_test_rsa executable.

---
+++ include/security/cryptmath.hpp
@@ -0,0 +1,6 @@
+int custom_rand();
+int mpmod(int base, int exponent, int modulus);
+int is_prime(int num);
+int next_pr(int num);
+int rand_prime();
+int rand_prime(int upper_limit);
---
+++ include/security/rsa.hpp
@@ -0,0 +1,33 @@
+struct Extended_Euclidean_Algorithm_container
+{
+    int var1;
+    int var2;
+};
+
+Extended_Euclidean_Algorithm_container eea(int a, int b);
+int find_inverse(int x, int y);
+
+class RSA_key_container
+{
+private:
+    bool valid;
+    int private_key;
+    int public_key;
+    int prime_p;
+    int prime_q;
+    int modulus;
+public:
+    RSA_key_container();
+    RSA_key_container(int p, int q);
+    bool get_validity();
+    void set_validity(bool validity);
+    int get_private_key();
+    void set_private_key(int new_private_key);
+    int get_public_key();
+    void set_public_key(int new_public_key);
+    int get_modulus();
+    void set_modulus(int new_modulus);
+};
+
+int encrypt_RSA(RSA_key_container container, int plaintext);
+int decrypt_RSA(RSA_key_container container, int ciphertext);
