c456c48b55c14cac82f50632c7b7729bad1bdc7c
Reuben John <reubenvjohn@gmail.com>
2016-10-04 15:34:05 +0530

    Added sqlite to CMakeLists.txt. TimelineServer uses preliminary SecureSocketDescriptor.

--- src/sync/timeline/TimelineServer.cpp
+++ src/sync/timeline/TimelineServer.cpp
@@ -9,107 +9,95 @@
 #include <iostream>
 #include <exception>
 
-#include <shared/SignalHandler.hpp>
-#include <security/SecureSocketManager.hpp>
+#include <boost/thread.hpp>
 
-namespace sftp{
+#include <sqlite3.h>
+
+#include <shared/SignalHandler.hpp>
+#include <shared/logger.hpp>
+#include <security/securesocket.hpp>
+
+namespace sftp {
 
 namespace sync {
 
 using namespace std;
-using namespace security;
+
+void customthread(SecureDataSocket acceptedSecureDataSocket) {
+	//acceptedSecureDataSocket.performDHExchange_asServer();
+	if (acceptedSecureDataSocket.getValidity() == true) {
+		cout << "Diffie-Hellman key exchange with client "
+				<< acceptedSecureDataSocket.getTargetAddrFromSockDesc() << ":"
+				<< acceptedSecureDataSocket.getTargetPortFromSockDesc()
+				<< " successful!" << endl;
+		do {
+			acceptedSecureDataSocket.readSecureSocket();
+			if (acceptedSecureDataSocket.getValidity() == false)
+				break;
+			cout << acceptedSecureDataSocket.getTargetAddrFromSockDesc() << ":"
+					<< acceptedSecureDataSocket.getTargetPortFromSockDesc()
+					<< " <--$ " << acceptedSecureDataSocket.getBuffer() << endl;
+			acceptedSecureDataSocket.writeSecureSocket();
+			if (acceptedSecureDataSocket.getValidity() == false)
+				break;
+			cout << acceptedSecureDataSocket.getTargetAddrFromSockDesc() << ":"
+					<< acceptedSecureDataSocket.getTargetPortFromSockDesc()
+					<< " -->$ " << acceptedSecureDataSocket.getBuffer() << endl;
+		} while (acceptedSecureDataSocket.getBuffer() != "quit"
+				&& acceptedSecureDataSocket.getValidity() == true);
+	}
+	cout << "Closing the connection from "
+			<< acceptedSecureDataSocket.getTargetAddrFromSockDesc() << ":"
+			<< acceptedSecureDataSocket.getTargetPortFromSockDesc() << endl;
+	acceptedSecureDataSocket.destroySecureSocket();
+}
 
 class TimelineServer {
 private:
-	int listenSocketDescriptor;
-	socklen_t clientAddrLen;
-	char buffer[256];
-
-	struct sockaddr_in servAddr, clientAddr;
+	SecureListenSocket serverSecureListenSocket;
 	SignalHandler sh;
 public:
 	TimelineServer(int portNo) :
 			sh() {
 		//sh.setupSignalHandlers();
-
-		int portNumber = portNo;
-		int n;
-
-		listenSocketDescriptor = socket(AF_INET, SOCK_STREAM, 0);
-		if (listenSocketDescriptor < 0) {
-			printf("Error: Socket failed!\n");
-			return;
-		} else {
-			printf("Successfully created socket: %d\n", listenSocketDescriptor);
-		}
-
-		memset(&servAddr, 0, sizeof(servAddr));
-		servAddr.sin_family = AF_INET;
-		servAddr.sin_addr.s_addr = INADDR_ANY;
-		servAddr.sin_port = htons(portNumber);
-
-		if (bind(listenSocketDescriptor, (struct sockaddr*) &servAddr,
-				sizeof(servAddr)) < 0) {
-			printf("Error: Bind failed!\n");
-			return;
-		} else {
-			printf("Successfully performed bind on socket\n");
-		}
-
-		if (listen(listenSocketDescriptor, 10) < 0) {
-			printf("Error: Listen failed!\n");
-			return;
-		} else {
-			printf("Successfully listening to socket\n");
-		}
-		clientAddrLen = sizeof(clientAddr);
+		serverSecureListenSocket.initSecureSocket();
+		serverSecureListenSocket.setSourceIPAddress("127.0.0.1");
+		serverSecureListenSocket.setSourcePortNumber(int_to_string(portNo));
+		serverSecureListenSocket.bindSecureSocket();
+		serverSecureListenSocket.listenSecureSocket();
 	}
 
-	void acceptNewConnection() {
-		printf("Waiting for connection\n");
-
-		int s = accept(listenSocketDescriptor, (struct sockaddr *) &clientAddr,
-				&clientAddrLen);
-
-		try {
-			bool bufferEmpty = false;
-			SecureSocketManager ssm(s);
-			while (!sh.gotExitSignal() && !bufferEmpty) {
-				try {
-					bzero(buffer, 256);
-					int n = ssm.readBuffer(buffer, 255);
-					buffer[n] = '\0';
-					printf("Read %d bytes: %s\n", n, buffer);
-					try {
-						ssm.writeBuffer(buffer, n);
-						printf("Wrote %d bytes\n", n);
-					} catch (SocketIOException e) {
-						LOG_ERROR<< "Socket write failed\n";
-					}
-				} catch (SocketIOException e) {
-					LOG_ERROR<< "Socket read failed";
-				} catch(SocketBufferEmptyException e) {
-					LOG_DEBUG<<"Socket buffer empty";
-					bufferEmpty = true;
-				}
-			}
-			LOG_DEBUG<<"Closing data socket: "<<s;
-			close(s);
-		} catch (shared::InvalidSocketException& e) {
-			LOG_ERROR<< e.what() << '\n';
-		} catch (std::exception& e) {
-			LOG_ERROR<< e.what() << '\n';
+	void acceptNewConnections() {
+		SecureDataSocket serverSecureDataSocket;
+		if (serverSecureListenSocket.getValidity() == false) {
+			cout << "Something went wrong!" << endl;
+			return; // TODO Throw Exception
+		} else {
+			cout << "Server is ready to accept connections from clients."
+					<< endl;
 		}
+		while (!sh.gotExitSignal()) {
+			cout << "Waiting to accept a connection..." << endl;
+			serverSecureDataSocket =
+					serverSecureListenSocket.acceptSecureSocket();
+			boost::thread t { customthread, serverSecureDataSocket };
+			//t.join();
+		}
+		cout << "Server program ending." << endl;
 	}
 
 	virtual ~TimelineServer() {
-		close(listenSocketDescriptor);
+		cout <<"Closing secure listen socket";
+		serverSecureListenSocket.destroySecureSocket();
 	}
 };
 
-}; /* synchronized namespace */
+}
+;
+/* sync namespace */
 
-};
+}
+;
 
 using namespace sftp::sync;
 
@@ -120,6 +108,6 @@ int main(int argc, char** argv) {
 	}
 	int portNo = atoi(argv[1]);
 	TimelineServer ts(portNo);
-	ts.acceptNewConnection();
+	ts.acceptNewConnections();
 	return 0;
 }
