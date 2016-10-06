06e712a5405ba0ac108d182ff1b21f4466d9b43c
root <soorya.annadurai@gmail.com>
2016-10-06 11:34:03 +0530

    Added support for null_deleter or empty_deleter. Done by using a selective include on boost libraries.

--- src/shared/logger.cpp
+++ src/shared/logger.cpp
@@ -11,7 +11,13 @@
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/shared_ptr.hpp>
-#include <boost/core/null_deleter.hpp>
+
+#define THRESHOLD_BOOST_VERSION 105400
+#if (BOOST_VERSION <= THRESHOLD_BOOST_VERSION)
+	#include <boost/log/utility/empty_deleter.hpp>
+#else
+	#include <boost/core/null_deleter.hpp>
+#endif
 #include <fstream>
 #include <ostream>
 
@@ -26,36 +32,73 @@ BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
 BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity",
 		logging::trivial::severity_level)
 
+
+#if (BOOST_VERSION <= THRESHOLD_BOOST_VERSION)
 BOOST_LOG_GLOBAL_LOGGER_INIT(logger, src::severity_logger_mt){
-src::severity_logger_mt<boost::log::trivial::severity_level> logger;
+	src::severity_logger_mt<boost::log::trivial::severity_level> logger;
 
-// add attributes
-logger.add_attribute("LineID", attrs::counter<unsigned int>(1));// lines are sequentially numbered
-logger.add_attribute("TimeStamp", attrs::local_clock());// each log line gets a timestamp
+	// add attributes
+	logger.add_attribute("LineID", attrs::counter<unsigned int>(1));// lines are sequentially numbered
+	logger.add_attribute("TimeStamp", attrs::local_clock());// each log line gets a timestamp
 
-// add a text sink
-typedef sinks::synchronous_sink<sinks::text_ostream_backend> text_sink;
-boost::shared_ptr<text_sink> sink = boost::make_shared<text_sink>();
+	// add a text sink
+	typedef sinks::synchronous_sink<sinks::text_ostream_backend> text_sink;
+	boost::shared_ptr<text_sink> sink = boost::make_shared<text_sink>();
 
-// add a logfile stream to our sink
-sink->locked_backend()->add_stream(boost::make_shared<std::ofstream>(LOGFILE));
+	// add a logfile stream to our sink
+	sink->locked_backend()->add_stream(boost::make_shared<std::ofstream>(LOGFILE));
 
-// add "console" output stream to our sink
-sink->locked_backend()->add_stream(boost::shared_ptr<std::ostream>(&std::clog, boost::null_deleter()));
+	// add "console" output stream to our sink
+	sink->locked_backend()->add_stream(boost::shared_ptr<std::ostream>(&std::clog, logging::empty_deleter()));
 
-// specify the format of the log message
-logging::formatter formatter = expr::stream
-<< std::setw(7) << std::setfill('0') << line_id << std::setfill(' ') << " | "
-<< expr::format_date_time(timestamp, "%Y-%m-%d, %H:%M:%S.%f") << " "
-<< "[" << logging::trivial::severity << "]"
-<< " - " << expr::smessage;
-sink->set_formatter(formatter);
+	// specify the format of the log message
+	logging::formatter formatter = expr::stream
+	<< std::setw(7) << std::setfill('0') << line_id << std::setfill(' ') << " | "
+	<< expr::format_date_time(timestamp, "%Y-%m-%d, %H:%M:%S.%f") << " "
+	<< "[" << logging::trivial::severity << "]"
+	<< " - " << expr::smessage;
+	sink->set_formatter(formatter);
 
-// only messages with severity >= SEVERITY_THRESHOLD are written
-sink->set_filter(severity >= SEVERITY_THRESHOLD);
+	// only messages with severity >= SEVERITY_THRESHOLD are written
+	sink->set_filter(severity >= SEVERITY_THRESHOLD);
 
-// "register" our sink
-logging::core::get()->add_sink(sink);
+	// "register" our sink
+	logging::core::get()->add_sink(sink);
 
-return logger;
+	return logger;
+	}
+#else
+BOOST_LOG_GLOBAL_LOGGER_INIT(logger, src::severity_logger_mt){
+	src::severity_logger_mt<boost::log::trivial::severity_level> logger;
+
+	// add attributes
+	logger.add_attribute("LineID", attrs::counter<unsigned int>(1));// lines are sequentially numbered
+	logger.add_attribute("TimeStamp", attrs::local_clock());// each log line gets a timestamp
+
+	// add a text sink
+	typedef sinks::synchronous_sink<sinks::text_ostream_backend> text_sink;
+	boost::shared_ptr<text_sink> sink = boost::make_shared<text_sink>();
+
+	// add a logfile stream to our sink
+	sink->locked_backend()->add_stream(boost::make_shared<std::ofstream>(LOGFILE));
+
+	// add "console" output stream to our sink
+	sink->locked_backend()->add_stream(boost::shared_ptr<std::ostream>(&std::clog, boost::null_deleter()));
+
+	// specify the format of the log message
+	logging::formatter formatter = expr::stream
+	<< std::setw(7) << std::setfill('0') << line_id << std::setfill(' ') << " | "
+	<< expr::format_date_time(timestamp, "%Y-%m-%d, %H:%M:%S.%f") << " "
+	<< "[" << logging::trivial::severity << "]"
+	<< " - " << expr::smessage;
+	sink->set_formatter(formatter);
+
+	// only messages with severity >= SEVERITY_THRESHOLD are written
+	sink->set_filter(severity >= SEVERITY_THRESHOLD);
+
+	// "register" our sink
+	logging::core::get()->add_sink(sink);
+
+	return logger;
 }
+#endif
